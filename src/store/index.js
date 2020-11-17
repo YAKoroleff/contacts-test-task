6b78d3892be5c5f35b90182e07a6a0b1165d64a4
YAKorolev <korolev.ya.v@gmail.com>
2020-11-17 14:20:27 +0300

    build store structure

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,37 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+//state data example
+let exampleState = [
+  [
+    { label: 'name',  value: 'Yaroslav' },
+    { label: 'Gihub', value: 'https://github.com/YAKorolev' }
+  ],
+  [
+    { label: 'name',  value: 'Some one else' },
+  ],
+]
+
 export default new Vuex.Store({
   state: {
+    //contacts: [],
+    contacts: exampleState,
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    appendContact (state, name) {
+      state.contacts.push([{label: 'name', value: name}])
+    },
+    removeContact(state, contactIndex) {
+      state.contacts.splice(contactIndex, 1)
+    },
+    appendNote (state, {contactIndex, note}) {
+      state.contacts[contactIndex].push(note)
+    },
+    editNote (state, {contactIndex, noteIndex, note}) {
+      state.contacts[contactIndex][noteIndex] = note
+    },
+    removeNote (state, {contactIndex, noteIndex}) {
+      state.contacts[contactIndex].splice(noteIndex, 1)
+    },
   }
 })
